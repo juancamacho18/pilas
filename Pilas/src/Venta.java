f5092b8586b2225728ccac72faf391f125a9f037
juancamacho18 <126798122+juancamacho18@users.noreply.github.com>
2024-10-11 16:18:49 -0500

    Add files via upload

---
+++ Pilas/src/Venta.java
@@ -0,0 +1,64 @@
+public class Venta {
+    private int numero_pedido;
+    private String fecha;
+    private int codigo;
+    private int cantidad;
+    private double precio;
+
+    public Venta(int numero_pedido, String fecha, int codigo, int cantidad, double precio) {
+        this.numero_pedido=numero_pedido;      
+        this.fecha=fecha;
+        this.codigo=codigo;
+        this.cantidad=cantidad;
+        this.precio=precio;
+    }
+
+    public int getNumero_pedido() {
+        return numero_pedido;
+    }
+
+    public void setNumero_pedido(int numero_pedido) {
+        this.numero_pedido = numero_pedido;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+      return "Articulo (numero de pedido "+getNumero_pedido()+", fecha: "+getFecha()+", codigo de producto: "+getCodigo()+", cantidad pedida: "+getCantidad()+", precio: $"+getPrecio()+")";  
+    }
+
+    
+
+    
+}
